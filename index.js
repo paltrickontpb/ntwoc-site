273a1260c09195d18eecb6fcb498bffa0edad7d8
paltrickontpb <paltrickontpb@gmail.com>
2018-08-28 00:53:03 +0530

    Added express backend

--- index.js
+++ index.js
@@ -1,14 +1,49 @@
-
 var express = require('express');
 var app = express();
 
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://sam:adminsam123@ds235302.mlab.com:35302/ntwoc";
+var dbName = "ntwoc"
+
+var bodyParser = require('body-parser')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+function pushDetails(_user, _name, _info, _abstract, _misc) {
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db(dbName);
+        var myobj = { 
+            name: _name, 
+            info: _info, 
+            abstract: _abstract,
+            misc: _misc
+        };
+        dbo.collection(_user).insertOne(myobj, function(err, res) {
+          if (err) throw err;
+          console.log("1 document inserted");
+          db.close();
+        });
+      });
+}
+
 var port = process.env.PORT || 3000;
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.use('/', express.static('public'))
 
 app.get('/api', function(req, res){
    res.send("Hello world!");
 });
+
+app.post('/api/register', (req,res)=>{
+    res.json('To be filled')
+})
 
 app.get('*', function(req, res){
     res.send("Error 404");
